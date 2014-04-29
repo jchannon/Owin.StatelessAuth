b8ec883ddc3abfd075fc4c73b5984bb48473321c
Jonathan Channon <jonathan.channon@gmail.com>
2014-04-29 15:36:03 +0100

    seperated it out

--- src/Owin.StatelessAuthExample/HomeModule.cs
+++ src/Owin.StatelessAuthExample/HomeModule.cs
@@ -16,6 +16,7 @@
             Post["/login"] = _ =>
             {
                 var user = this.Bind<UserCredentials>();
+                //Verify user/pass
                 if (user.User != "fred" && user.Password != "securepwd")
                 {
                     return 401;
@@ -46,19 +47,5 @@
     {
         public string User { get; set; }
         public string Password { get; set; }
-    }
-
-    public interface IJwtWrapper
-    {
-        string Encode(object payload, string key, JwtHashAlgorithm algorithm);
-        //object DecodeToObject(string token, string key, bool verify = true);
-    }
-
-    public class JwtWrapper : IJwtWrapper
-    {
-        public string Encode(object payload, string key, JwtHashAlgorithm algorithm)
-        {
-            return JsonWebToken.Encode(payload, key, algorithm);
-        }
     }
 }
